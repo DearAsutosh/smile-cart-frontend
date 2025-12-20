c57250aba7aa604e8b1402fc6ac352a4feeadf16
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-12-20 17:45:01 +0530

    Utilized updater function to set state values

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,34 +1,56 @@
 import { useState } from "react";
 
-import classNames from "classnames";
+// import classNames from "classnames";
+import { Left, Right } from "neetoicons";
+import { Button } from "neetoui";
 // import { Left, Right } from "neetoicons";
 // import { Button } from "neetoui";
 
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  // const handleNext = () => {
-  //   setCurrentIndex((currentIndex + 1) % imageUrls.length);
-  // };
+  const handleNext = () => {
+    setCurrentIndex((currentIndex + 1) % imageUrls.length);
+    console.log(currentIndex);
+  };
 
-  // const handlePrevious = () => {
-  //   setCurrentIndex((currentIndex - 1 + imageUrls.length) % imageUrls.length);
-  // };
+  const handlePrevious = () => {
+    setCurrentIndex((currentIndex - 1 + imageUrls.length) % imageUrls.length);
+  };
 
   return (
-    <div className="flex flex-col items-center">
-      <img alt={title} src={imageUrls[currentIndex]} />
-      <div className="mt-2 flex space-x-1 ">
-        {imageUrls.map((_, index) => (
-          <span
-            key={index}
-            className={classNames(
-              "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
-              { "neeto-ui-bg-black": index === currentIndex }
-            )}
-            onClick={() => setCurrentIndex(index)}
-          />
-        ))}
-      </div>
+    // <div className="flex flex-col items-center">
+    //   <img alt={title} src={imageUrls[currentIndex]} />
+    //   {/* <div className="mt-2 flex space-x-1 ">
+    //     {imageUrls.map((_, index) => (
+    //       <span
+    //         key={index}
+    //         className={classNames(
+    //           "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
+    //           { "neeto-ui-bg-black": index === currentIndex }
+    //         )}
+    //         onClick={() => setCurrentIndex(index)}
+    //       />
+    //     ))}
+    //   </div> */}
+    // </div>
+    <div className="flex items-center">
+      <Button
+        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+        icon={Left}
+        style="text"
+        onClick={handlePrevious}
+      />
+      <img
+        alt={title}
+        className="max-w-56 h-56 max-h-56 w-56"
+        src={imageUrls[currentIndex]}
+      />
+      <Button
+        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+        icon={Right}
+        style="text"
+        onClick={handleNext}
+      />
     </div>
   );
 };
