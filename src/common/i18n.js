acd82bd37e10c0b4fb40305822ccd74dc214a9bd
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-12-27 11:17:41 +0530

    Implemented localization to smile cart

--- src/common/i18n.js
+++ src/common/i18n.js
@@ -4,7 +4,7 @@ import en from "translations/en.json";
 
 i18n.use(initReactI18next).init({
   resources: { en: { translation: en } },
-  fallbackLng: en,
+  fallbackLng: "en",
 });
 
 export default i18n;
